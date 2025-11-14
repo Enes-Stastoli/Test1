ff005ab4095e1f267c02f1fb0810de96d5b97550
Enes-Stastoli <estastoli@umt.edu.al>
2025-11-14 09:36:31 +0100

    Fix printf statement to include newline

--- arraytest.c
+++ arraytest.c
@@ -4,7 +4,7 @@ int main(){
   int array[]={1,2,3,4,5};
 
   for(int i=0; i<4;i++){
-      printf("Elementet ne varg jane: %d", array[i]);
+      printf("Elementet ne varg jane: %d\n", array[i]);
   }
 
   return 0;
