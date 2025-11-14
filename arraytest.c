e245cabd42b4c9c24d4751dad7b8add326f6f682
Enes-Stastoli <estastoli@umt.edu.al>
2025-11-14 09:39:08 +0100

    Add files via upload

--- arraytest.c
+++ arraytest.c
@@ -2,6 +2,7 @@
 
 int main(){
   int array[]={1,2,3,4,5};
+  puts("Program ne C per te printuar elementet e nje vargu.\n");
 
   for(int i=0; i<4;i++){
       printf("Elementet ne varg jane: %d\n", array[i]);
